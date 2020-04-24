fe484e9c30821405760aa6c368304d08fcae248a
dtnewton <dnewton7@uncc.edu>
2020-04-24 00:19:24 -0400

    initial backup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
+class App extends React.Component {
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" render = { props => { return ( <h1>Login</h1>)}}/>
+            <Route path="/" render = { props => { return ( <h1>Root</h1>)}}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
