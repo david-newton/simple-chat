d982e0a5e8792690a944559c46a2d92a6b1d99f5
David Newton <david.t.newton@gmail.com>
2020-05-07 21:28:02 -0400

    5/7/2020

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-
-
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as ChatActions from "./store/actions/chatActions";
 
 class App extends React.Component {
-
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.setupSocket();
+  }
 
   render() {
     return (
       <div className="App">
         <BrowserRouter>
           <Switch>
-            <Route path="/login" render = { props => { return ( <h1>Login</h1>)}}/>
-            <Route path="/" render = { props => { return ( <h1>Root</h1>)}}/>
+            <Route
+              path="/login"
+              render={(props) => {
+                return <h1>Login</h1>;
+              }}
+            />
+            <Route
+              path="/"
+              render={(props) => {
+                return <h1>Root</h1>;
+              }}
+            />
           </Switch>
         </BrowserRouter>
       </div>
-    )
+    );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  ...state.auth,
+  ...state.chat,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setupSocket: () => {
+    dispatch(ChatActions.setupSocket());
+  },
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
